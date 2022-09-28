6c55f9ffc0cfb14564aef62db53f59134eff160d
BORNKING16 <ige.adeyemi1993@gmail.com>
2022-09-28 05:28:11 -0700

    hhh

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,6 @@
 
 
 def roman_to_int(roman_string):
-    """Converts a roman numeral to an integer."""
     if (not isinstance(roman_string, str) or
             roman_string is None):
         return (0)
@@ -28,4 +27,4 @@ def roman_to_int(roman_string):
 
         else:
             num += roman_dict[roman_string[i]]
-    return num
+    return (num)
