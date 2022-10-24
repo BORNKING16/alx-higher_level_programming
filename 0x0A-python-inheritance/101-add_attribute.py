f540154c25ab0c41717cbbac37af6a71b6beaf8a
BORNKING16 <ige.adeyemi1993@gmail.com>
2022-10-24 11:10:37 -0700

    rechecked

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,36 +1,10 @@
 #!/usr/bin/python3
-"""Module 11-square.
-Creates a Square class.
-"""
+"""this module defines a function that adds attributes to objects"""
 
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-Rectangle = __import__('9-rectangle').Rectangle
-
-
-class Square(Rectangle):
-    """Represents a square.
-    Private instance attribute size.
-    Public method area().
-    Inherits from Rectangle.
+def add_attribute(obj, att, value):
+    """Add a new attribute to an object if possible
     """
-
-    def __init__(self, size):
-        """Initializes a Square.
-        Args:
-            - size: size of the square
-        """
-
-        self.integer_validator("size", size)
-        super().__init__(size, size)
-        self.__size = size
-
-    def __str__(self):
-        return str("[Square] {}/{}".format(self.__size, self.__size))
-
-    def area(self):
-        """Computes the area of a Square instance.
-        Overwrites the area() method from Rectangle.
-        """
-
-        return self.__size ** 2
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(obj, att, value)
